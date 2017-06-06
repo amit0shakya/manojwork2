3a5f63a05a570eb6fc8961b5da707b4d95eb9294
amit shakya <amit.shakya@webitude.com>
2017-06-05 18:35:25 -0700

    update fresher form

--- client/src/app/registration/candidate/freshersform/fresherregisteration.component.ts
+++ client/src/app/registration/candidate/freshersform/fresherregisteration.component.ts
@@ -14,6 +14,22 @@ export class FresherregisterationComponent implements OnInit {
 captchadata={val1:0,val2:0,method:''};
 captchadataanswer=null;
 
+
+educationalfield={class:'',school:'',univ:'',duration:{from:'',to:''},marks:''};
+educationalinfo=[]
+
+techqualfield={qual:"",univ:"",degree:"",duration:{from:'',to:''},marks:''};
+techqualinfo=[this.techqualfield];
+
+/*
+
+<th>Qualification</th>
+                    <th>Discription</th>
+                    <th>Degree</th>
+                    <th>Duration</th>
+                    <th>Marks%</th>
+*/ 
+
 userData= new candidateData('','','','','','','');
 
 indianstates=[{"title":"Andhra Pradesh"},{"title":"Arunachal Pradesh"},{"title":"Assam"},{"title":"Bihar"},
@@ -43,6 +59,26 @@ browsedfilename="Upload Resume";
       //Do nothing if user dosent choose any file;
     }else{
       this.browsedfilename = filname.target.value.replace("C:\\fakepath\\", "");
+    }
+  }
+
+  addTechQual(){
+    this.techqualinfo.push(this.techqualfield);
+  }
+
+  removetechQual(index){
+    if(this.techqualinfo.length>0){
+      this.techqualinfo.splice(index,1);
+    }
+  }
+
+  addQual(){
+    this.educationalinfo.push(this.educationalfield)
+  }
+
+  removeQual(index){
+    if(this.educationalinfo.length>0){
+      this.educationalinfo.splice(index,1);
     }
   }
 
